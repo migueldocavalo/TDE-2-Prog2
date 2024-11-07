8477c7dbabdf8f1832bd4a95dd50c3a2df10de65
miguel garcia <miguel_garcia5555@hotmail.com>
2024-11-07 19:52:52 -0300

    Update questão 7 .C

--- questão 7 .C
+++ questão 7 .C
@@ -1,1 +1,10 @@
-
+int main() {
+    int i = 0;
+	char str[100];
+    scanf("%s", str);
+    for ( i = 0; str[i] != '\0'; i++) {
+        str[i] = toupper(str[i]);
+    }
+    printf("%s\n", str);
+    return 0;
+}
