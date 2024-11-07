22de26d2e24110f0c409927cda193e6bd124cbfc
miguel garcia <miguel_garcia5555@hotmail.com>
2024-11-07 19:53:34 -0300

    Update questão 9 .C

--- questão 9 .C
+++ questão 9 .C
@@ -1,1 +1,13 @@
-
+int main() {
+    int i = 0;
+	char str[100];
+    int cont = 0;
+    scanf("%s", str);
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            cont++;
+        }
+    }
+    printf("%d\n", cont);
+    return 0;
+}
