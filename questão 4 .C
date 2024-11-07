8316e306c3885ac82a4dfd625ab76af337219fe0
miguel garcia <miguel_garcia5555@hotmail.com>
2024-11-07 19:51:45 -0300

    Update questão 4 .C

--- questão 4 .C
+++ questão 4 .C
@@ -1,1 +1,14 @@
+int main() {
+    int i = 0;
+	char str[100];
+    int count = 0;
+    scanf("%s", str);
+    for ( i = 0; str[i] != '\0'; i++) {
+        if (str[i] == 'a' || str[i] == 'A') {
+            count++;
+        }
+    }
+    printf("%d\n", count);
+    return 0;
+}
 
