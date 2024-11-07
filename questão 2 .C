49ff82ccf42a454af7ea8dcd7f46612687f27fe7
miguel garcia <miguel_garcia5555@hotmail.com>
2024-11-07 19:50:46 -0300

    Update questão 2 .C

--- questão 2 .C
+++ questão 2 .C
@@ -1,1 +1,11 @@
-
+int main() {
+    char str1[100], str2[100];
+    scanf("%s", str1);
+    scanf("%s", str2);
+    if (strcmp(str1, str2) == 0) {
+        printf("As strings são iguais.\n");
+    } else {
+        printf("As strings são diferentes.\n");
+    }
+    return 0;
+}
