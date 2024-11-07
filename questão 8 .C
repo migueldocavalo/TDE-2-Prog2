fa405ab9fad0c69b454dc29fa9288ccb8be2b2df
miguel garcia <miguel_garcia5555@hotmail.com>
2024-11-07 19:53:13 -0300

    Update questão 8 .C

--- questão 8 .C
+++ questão 8 .C
@@ -1,1 +1,14 @@
-
+int main() {
+    int i = 0;
+	char str[100];
+    int cont = 0;
+    scanf("%s", str);
+    for ( i = 0; str[i] != '\0'; i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || 
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+            cont++;
+        }
+    }
+    printf("%d\n", cont);
+    return 0;
+}
