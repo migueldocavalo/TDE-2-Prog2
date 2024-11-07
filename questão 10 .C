3e5cdd00fde4f38af68e258b48838b054f97bb27
miguel garcia <miguel_garcia5555@hotmail.com>
2024-11-07 19:54:04 -0300

    Update questão 10 .C

--- questão 10 .C
+++ questão 10 .C
@@ -1,1 +1,20 @@
+int main() {
+    int i = 0;
+	char str[100];
+    int letras = 0, digito = 0, especial = 0;
+    scanf("%s", str);
+    for ( i = 0; str[i] != '\0'; i++) {
+        if (isalpha(str[i])) {
+            letras++;
+        } else if (isdigit(str[i])) {
+            digito++;
+        } else {
+            especial++;
+        }
+    }
+    printf("Letras: %d\n", letras);
+    printf("Dígitos: %d\n", digito);
+    printf("Especiais: %d\n", especial);
+    return 0;
+}
 
